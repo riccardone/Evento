b2f8fc6a9571680c3efd1e6ecf775bcbcca0bc0d
riccardone <riccardo@dinuzzo.it>
2017-01-22 14:15:53 +0000

    added test project using xunit with one initial test

---
+++ src/EventStore.Tools.Infrastructure.Tests/BusTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xunit;
+
+namespace EventStore.Tools.Infrastructure.Tests
+{
+    public class BusTests
+    {
+        [Fact]
+        public void CreateBus()
+        {
+            var bus = new Bus(new InMemoryDomainRespository(), null, null);
+            bus.RegisterCommandHandler(new FakeHandler());
+            Assert.True(bus.CanHandle(typeof(FakeCommand)));
+        }
+
+        class FakeAggregate : AggregateBase
+        {
+            public override string AggregateId { get; }
+
+            public FakeAggregate(string id)
+            {
+                AggregateId = id;
+            }
+        }
+
+        class FakeCommand : ICommand { }
+
+        class FakeHandler : IHandle<FakeCommand>
+        {
+            public IAggregate Handle(FakeCommand command)
+            {
+                return new FakeAggregate(Guid.NewGuid().ToString());
+            }
+        }
+    }
+}
