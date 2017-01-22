fd257d3c892c68f1981752f6a0ade3bcfe41e9f9
riccardone <riccardo@dinuzzo.it>
2017-01-22 12:43:42 +0000

    Added test project

---
+++ src/EventStore.Tools.Infrastructure.Tests/BusTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventStore.Tools.Infrastructure.Tests
+{
+    [TestClass]
+    public class BusTests
+    {
+        [TestMethod]
+        public void CreateBus()
+        {
+            var bus = new Bus(new InMemoryDomainRespository(), null, null);
+            bus.RegisterCommandHandler(new FakeHandler());
+            Assert.IsTrue(bus.CanHandle(typeof(FakeCommand)));
+        }
+
+        class FakeAggregate : AggregateBase
+        {
+            public override string AggregateId { get; }
+
+            public FakeAggregate(string id)
+            {
+                AggregateId = id;
+            }
+        }
+
+        class FakeCommand : ICommand {}
+
+        class FakeHandler : IHandle<FakeCommand>
+        {
+            public IAggregate Handle(FakeCommand command)
+            {
+                return new FakeAggregate(Guid.NewGuid().ToString());
+            }
+        }
+    }
+}
