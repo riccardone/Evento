f86f49c8cdd5b6d4415d7b5add40d973dc39c736
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2018-03-21 23:08:44 +0000

    implemented method to delete the aggregate-stream from the disk

--- src/Evento/DomainRepositoryBase.cs
+++ src/Evento/DomainRepositoryBase.cs
@@ -7,6 +7,7 @@ namespace Evento
         public abstract IEnumerable<Event> Save<TAggregate>(TAggregate aggregate) where TAggregate : IAggregate;
         public abstract TResult GetById<TResult>(string id) where TResult : IAggregate, new();
         public abstract TResult GetById<TResult>(string id, int eventsToLoad) where TResult : IAggregate, new();
+        public abstract void DeleteAggregate<TAggregate>(string correlationId, bool hard);
         protected int CalculateExpectedVersion<T>(IAggregate aggregate, List<T> events)
         {
             var expectedVersion = aggregate.Version - events.Count;
