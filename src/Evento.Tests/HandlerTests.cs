0514b22da30df8b41eadc04c761506a533338909
riccardone <riccardo@dinuzzo.it>
2019-01-20 19:44:34 +0000

    Updated EventStore.Client to v5; Refactored and updated tests project; set up new ci

--- src/Evento.Tests/HandlerTests.cs
+++ src/Evento.Tests/HandlerTests.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using Infrastructure.Tests.Fakes;
+using Evento.Tests.Fakes;
 using NUnit.Framework;
 
-namespace Infrastructure.Tests
+namespace Evento.Tests
 {
     [TestFixture]
     public class HandlerTests
