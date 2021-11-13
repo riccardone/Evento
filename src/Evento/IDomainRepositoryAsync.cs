5aa62853aae2b1017c3264b7e6cc1c6a600ba333
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2021-11-13 13:00:18 +0000

    add IDomainRepositoryAsync and it's implementation

---
+++ src/Evento/IDomainRepositoryAsync.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Evento
+{
+    /// <summary>
+    /// This interface define the methods that a DomainRepository must expose
+    /// </summary>
+    public interface IDomainRepositoryAsync
+    {     
+        /// <summary>
+        /// This asynchronous method save all the uncommitted events of the passed aggregate 
+        /// </summary>
+        /// <param name="aggregate">The Aggregate containing uncommitted events</param>
+        /// <returns>All the events that have been sent to EventStore</returns>
+        Task<IEnumerable<Event>> SaveAsync<TAggregate>(TAggregate aggregate) where TAggregate : IAggregate;
+
+        /// <summary>
+        /// This method can be used to retrieve the Aggregate in it's current state (all previous events applied)
+        /// </summary>
+        /// <typeparam name="TResult">IAggregate</typeparam>
+        /// <param name="correlationId">The unique identifier of the requested Aggregate</param>
+        /// <returns>The Aggregate retrieved from the store in it's current state (all previous events applied)</returns>
+        Task<TResult> GetByIdAsync<TResult>(string correlationId) where TResult : IAggregate, new();
+
+        /// <summary>
+        /// This method can be used to retrieve the Aggregate in it's current state (all previous events applied)
+        /// </summary>
+        /// <typeparam name="TResult">IAggregate</typeparam>
+        /// <param name="correlationId">The unique identifier of the requested Aggregate</param>
+        /// <param name="eventsToLoad">The number of events to apply if you don't need to load the full history</param>
+        /// <returns>The Aggregate retrieved from the store in it's current state (all previous events applied)</returns>
+        Task<TResult> GetByIdAsync<TResult>(string correlationId, int eventsToLoad) where TResult : IAggregate, new();
+
+        /// <summary>
+        /// This method can be used to remove an entire aggregate and all the events within its stream from the disk
+        /// </summary>
+        /// <param name="correlationId">The unique identifier of the requested Aggregate</param>
+        /// <param name="hard">If true it will be impossible to recreate the stream with the same correlationId</param>
+        Task DeleteAggregateAsync<TAggregate>(string correlationId, bool hard);
+    }
+}
