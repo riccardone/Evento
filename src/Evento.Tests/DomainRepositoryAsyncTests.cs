6b782981ff729da2cd67ed001e12e5e0973273fa
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2021-11-14 21:28:23 +0000

    Handle async (#27)
    * implement ihandleasync
    
    * update package ver and info
    
    * remove file
    
    * add IDomainRepositoryAsync and it's implementation
    
    * obsolete old async method; fix ver in EventStoreDomainRepository nuget
    
    * ...

---
+++ src/Evento.Tests/DomainRepositoryAsyncTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Evento.Repository;
+using Evento.Tests.Fakes;
+using EventStore.ClientAPI;
+using Moq;
+using NUnit.Framework;
+
+namespace Evento.Tests
+{
+    [TestFixture]
+    public class DomainRepositoryAsyncTests
+    {
+        [Test]
+        public void When_I_save_an_aggregate_I_should_receive_the_saved_events()
+        {
+            // Assign
+            const string correlationId = "correlationidexample-123";
+            const string testString = "test";
+            var cmd = new CreateFakeCommand(testString, new Dictionary<string, string>{{"$correlationId", correlationId}});
+            var mockConnection = new Mock<IEventStoreConnection>();
+            var repository = new EventStoreDomainRepositoryAsync("domain", mockConnection.Object);
+
+            // Act
+            var results = repository.SaveAsync(new FakeHandler().Handle(cmd)).Result;
+
+            // Assert
+            Assert.IsTrue(((FakeAggregateCreated)results.Single()).Metadata["$correlationId"].Equals(correlationId));
+        }
+    }
+}
---
+++ src/Evento.Tests/HandlerAsyncTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Evento.Tests.Fakes;
+using NUnit.Framework;
+
+namespace Evento.Tests
+{
+    [TestFixture]
+    public class HandlerAsyncTests
+    {
+        [Test]
+        public void When_I_receive_a_command_I_should_raise_created_event()
+        {
+            // Assign
+            const string correlationId = "correlationidexample-123";
+            const string testString = "test";
+            var cmd = new CreateFakeCommand(testString,
+                new Dictionary<string, string> {{"$correlationId", correlationId}});
+
+            // Act
+            var results = new FakeHandler().HandleAsync(cmd).Result;
+
+            // Assert
+            Assert.IsTrue(((FakeAggregateCreated) results.UncommitedEvents().First()).TestString.Equals(testString));
+        }
+    }
+}
