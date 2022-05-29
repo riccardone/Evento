6f7833f37c1c4b8f13aad1ba1f5ba082b82fb215
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2022-05-29 18:59:30 +0100

    grpc client (#29)
    * grpc client
    
    * implement DomainRepository using the EventStore Grpc client

---
+++ src/Evento.TestClient.Grpc/TestCreated.cs
@@ -0,0 +1,14 @@
+﻿namespace Evento.TestClient.Grpc
+{
+    public class TestCreated : Event
+    {
+        public string Id { get; }
+        public IDictionary<string, string> Metadata { get; }
+
+        public TestCreated(string id, IDictionary<string, string> metadata)
+        {
+            Id = id;
+            Metadata = metadata;
+        }
+    }
+}
